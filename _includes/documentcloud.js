152ed4f34473f9b15124bae50d5293138289a297
palewire <ben.welsh@gmail.com>
2018-06-20 17:24:47 -0700

    Documentcloud generator

---
+++ _includes/documentcloud.js
@@ -0,0 +1,13 @@
+var dcParse = function (url) {
+    // https://www.documentcloud.org/documents/254013-attehn61.html
+    return url.split("documents/")[1].split(".html")[0];
+}
+
+function creatDocumentCloudEmbed(){
+    var url = $("#documentcloud-url").val();
+    var slug = dcParse(url);
+    var template = _.template($("#documentcloud-jst").html());
+    var html = template({slug: slug});
+    $("#documentcloudembedNote").fadeIn();
+    $("#documentcloudembedCode").html(embedLabel + "<textarea rows='12' class='codebox'>" + embedContainerCSS + embedContainerDivOpen + html + embedContainerDivClose + "</textarea>");
+}
