641b4c9dac3376a8436ae8899def45602d0cb7a6
Castleman <tcastleman@la-dgt-54775.usa.tribune.com>
2019-03-01 10:17:46 -0800

    editorial changes

--- _includes/column-one-representation.js
+++ _includes/column-one-representation.js
@@ -1,6 +1,15 @@
 function createColumnOneRepresentation(){
     var story_url = $("#story_url").val();
     var photo_url = $("#photo_url").val();
+    var repDimensions = $("#repDimensions").val();
+
+    //assign correct padding
+    if (repDimensions == "16x9"){
+        var repSizeCSS = "16x9 {padding-bottom: 56.25%;}";
+        }
+        if (repDimensions == "4x3"){
+        var repSizeCSS = "4x3 {padding-bottom: 75%;}";
+        }
 
     var $return = '<style type="text/css" media="screen"> \
 #hptag { \
@@ -14,7 +23,7 @@ border-bottom: 3px solid #990000; \
 <div class="width-100 flex-container-column"> \
 <figure class=""> \
 <div class=" img-container aspect-ratio-4x3"> \
-<a href="' + story_url + '"> <img class="full-width" alt="" src="' + photo_url + '"> </a> \
+<a href="' + story_url + '"> <img class="full-width ' + repDimensions + '" alt="" src="' + photo_url + '"> </a> \
 </div> \
 </figure> \
 <ul id="hptag" class="tag-list-wrapper list-unstyled flex-container-row spaced spaced-sm spaced-top justify-center"> \
