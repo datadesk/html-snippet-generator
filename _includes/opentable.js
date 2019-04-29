dfbc5b8920366760a552346d8dab83d77304fbe8
Castleman <tcastleman@la-dgt-54775.usa.tribune.com>
2019-04-29 12:02:41 -0700

    tentatively working

--- _includes/opentable.js
+++ _includes/opentable.js
@@ -1,18 +1,16 @@
 function openTableEmbed(){
     var openTableURL = $("#openTable-url").val();
-    var escapedOTURL = openTableURL.replace(/\"/g,'\'');
-    var escapedOTURLCode = escapedOTURL.replace(/</g,'&lt;');
-    var escapedOTURLCodeFinal = escapedOTURLCode.replace(/>/g,'&gt;');
+    openTableURL += '?ref=18113';
 
+    var returnHTML = '<hr style="border: 0.5px solid black;"><img src="assets/img/OT_logos/Logo_horizontal_RGB.jpg"><span style="padding-left:100px; padding-right: 100px; width: 212px;height: 12px;font-family: BentonGothic;font-size: 12px;font-weight: bold;font-style: normal;font-stretch: normal;line-height: 1.17;letter-spacing: normal;text-align: center;color: #333333;">Make a reservation at this restaurant.</span><a href="' + openTableURL + '"><span style="display:inline-block; width:200px; height:50px; border-radius:4px; background-color: #d83947;"><h1 style="width: 200px;height: 18px;font-family: BentonGothic;font-size: 18px;font-weight: bold;font-style: normal;font-stretch: normal;line-height: 1.22;letter-spacing: normal;text-align: center;color: #ffffff;">Find a Table</h1></span></a><hr style="border: 0.5px solid black;">';
     //alert(escapediFrameURL);
-    $("#genericembedNote").fadeIn();
-    $("#genericembedCode").html( embedLabel + "<textarea rows='12' class='codebox'>" + embedContainerCSS + embedContainerDivOpen + escapedOTURLCodeFinal + embedContainerDivClose + "</textarea>");
-    $("#genericpreview").html( previewLabel + previewPrefix + escapedOTURL + previewSuffix );
+    $("#openTableembedCode").html( embedLabel + "<textarea rows='12' class='codebox'>" + returnHTML + "</textarea>");
+    $("#openTablepreview").html( previewLabel + previewPrefix + returnHTML + previewSuffix );
 }
 
 function openTablecopyText(){
-    var content = $("#genericembedCode textarea.codebox");
+    var content = $("#openTableembedCode textarea.codebox");
     content.select();
     document.execCommand("copy");
-    $("#iframe-copier").html("Copied!");
+    $("#openTable-copier").html("Copied!");
 }
